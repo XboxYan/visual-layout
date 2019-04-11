1410c0de992ba790a8e78f78627c3537b7ef89db
XboxYan <yanwenbin1991@live.com>
2019-04-11 18:05:23 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+  class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
+      );
+    }
+  }
+
+  export default App;
